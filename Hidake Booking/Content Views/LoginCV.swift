9394d2469ef1f88d797f96becca7b43cdb6a58de
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-03-10 08:15:02 +0800

    Initial Commit

---
+++ Hidake Booking/Content Views/LoginCV.swift
@@ -0,0 +1,20 @@
+//
+//  LoginCV.swift
+//  Hidake Booking
+//
+//  Created by Jia-Jiuan Tsai on 2021-03-10.
+//
+
+import SwiftUI
+
+struct LoginCV: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginCV_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginCV()
+    }
+}
---
+++ Hidake Booking/Hidake_BookingApp.swift
@@ -0,0 +1,25 @@
+//
+//  Hidake_BookingApp.swift
+//  Hidake Booking
+//
+//  Created by Jia-Jiuan Tsai on 2021-03-10.
+//
+
+import SwiftUI
+
+@main
+struct Hidake_BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+}
